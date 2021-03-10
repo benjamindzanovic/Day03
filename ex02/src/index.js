00273ec6032c1a07d3524db409d493e3377be9d1
benjamindzanovic <dzanovicbenjamin@gmail.com>
2021-03-11 00:12:04 +0100

    js file ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,7 @@
+// This is a single line Javascript comment
+
+/* document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!");
+document.write("This is a multi-line comment!"); */
