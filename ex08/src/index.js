78792f3720c385acb57f1c24ce9aa9e9cde9e0a8
benjamindzanovic <dzanovicbenjamin@gmail.com>
2021-03-11 02:27:13 +0100

    js file ex 08

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var x = 17
+    var y = 18
+    var sum = x + y;
+return sum;
+    
+    }
+    
+    console.log(main(17, 18));
+    module.exports = (main);
