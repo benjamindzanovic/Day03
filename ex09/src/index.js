be8d0f9bb1053fe3df89ea9c5448d1140616c70e
benjamindzanovic <dzanovicbenjamin@gmail.com>
2021-03-11 02:32:41 +0100

    js file ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var x = 17
+    var y = 2
+    var difference = x - y;
+return difference;
+    
+    }
+    
+    console.log(main(17, 2));
+    module.exports = (main);
