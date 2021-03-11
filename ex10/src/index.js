ce1f16d27acfdd77b5c4dddc7349333bb2eb8ed2
benjamindzanovic <dzanovicbenjamin@gmail.com>
2021-03-11 02:37:37 +0100

    js file ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var x = 5
+    var y = 9
+    var product = x * y;
+return product;
+    
+    }
+    
+    console.log(main(5, 9));
+    module.exports = (main);
